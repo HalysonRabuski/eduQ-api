db74f83bc57be59ed179f3ca020f5560f2e2ef84
halysonrabuski <rabuskihalyson@gmail.com>
2021-05-17 01:57:03 -0300

    seeds fixes

--- src/seeds/PatientSeeder.ts
+++ src/seeds/PatientSeeder.ts
@@ -1,22 +1,22 @@
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 import Patient from '../models/Patient';
 import patients from '../utils/patients.json';
 
-mongoose.connect(
-  `mongodb+srv://halyson:YVd@KQss0@cluster0.dy9wc.mongodb.net/eduq?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-);
+dotenv.config();
+
+mongoose.connect(`${process.env.MONGODB_URI}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 // const is
 
 async function seedPatients() {
   try {
-    console.log('Clearing Database');
+    console.log('Clearing Patients Document');
     await Patient.deleteMany({});
-    console.log('Seeding Database with 500 items');
+    console.log('Seeding Patients Document with 500 items');
     await Patient.insertMany(patients.results);
     console.log('Databse Seeded successfully');
     console.log('Disconnecting');
